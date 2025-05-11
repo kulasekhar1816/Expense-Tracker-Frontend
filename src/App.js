163b7bbf87f3e086eccc4c350f267d5cf04eed4d
kulasekhar1816 <107290184+kulasekhar1816@users.noreply.github.com>
2025-05-11 19:10:02 +0530

    Update App.js
    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import './App.css';
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 import Dashboard from "./Pages/Dashboard";
 
 function App() {
   const [msg, setMsg] = useState("");
+
   // useEffect(() => {
   //   axios({
   //     method: "get",
   //     url: "http://localhost:8000/expenses",
-  //     // data: {
-  //     //   response: {
-  //     //     name: 'kulasekhar',
-  //     //   }
-  //     // },
   //   })
-  //   .then((response) => setMsg(response.data[0]['title']))
+  //     .then((response) => setMsg(response.data[0]['title']));
+  // }, [])
 
-  // },[])
   return (
     <div>
       <Router>
         <Routes>
+          <Route path='/' element={<Navigate to="/login" />} /> {/* Redirect root path to /login */}
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
           <Route path='/dashboard' element={<Dashboard />} />
